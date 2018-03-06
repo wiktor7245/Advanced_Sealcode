05ec5bd084bafcaa53678ebc224abac13886ef3e
wiktor7245 <wiktor7245@o2.pl>
2018-03-06 12:43:14 +0100

    zad1

---
+++ Lesson_01/exercise_112.js
@@ -0,0 +1,10 @@
+for (var i = 0; i < 5; i++) {
+    /*setTimeout(function() {
+        console.log(i);
+    }, 100);*/
+    (function (i) {
+      setTimeout(function() {
+          console.log(i);
+      }, 100);
+    })(i);
+}
