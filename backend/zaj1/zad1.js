b5251a402f54a1a5cb0f9d11c721e8656ea5b53d
wiktor7245 <wiktor7245@o2.pl>
2018-04-03 11:06:35 +0200

    homework

---
+++ backend/zaj1/zad1.js
@@ -0,0 +1,12 @@
+//dopisać obsługę błędu związanego z argumentami i ogarnąc to dopisywanie po określonym czasie
+
+var fs = require('fs');
+var counter = process.argv[2];
+var doc = process.argv[3];
+
+
+//append dodaje cos do pliku, a w przypadku, gdy nie istnieje, tworzy go
+fs.appendFile(doc, 'Hello content!\n', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
