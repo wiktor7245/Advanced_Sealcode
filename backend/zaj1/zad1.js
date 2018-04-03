ca740ac85f437e12711c4d8cc1f0d298c06545ea
wiktor7245 <wiktor7245@o2.pl>
2018-04-03 12:30:35 +0200

    hm

--- backend/zaj1/zad1.js
+++ backend/zaj1/zad1.js
@@ -1,4 +1,4 @@
-//dopisać obsługę błędu związanego z argumentami i ogarnąc to dopisywanie po określonym czasien
+//dopisać obsługę błędu związanego z argumentami
 
 var fs = require('fs');
 var counter = process.argv[2];
@@ -6,7 +6,7 @@ var doc = process.argv[3];
 
 
 //append dodaje cos do pliku, a w przypadku, gdy nie istnieje, tworzy go
-fs.appendFile(doc, 'Hello content!\n', function (err) {
+setTimeout(() => fs.appendFile(doc, 'Hello content!\n', function (err) {
     if (err) throw err;
     console.log('Saved!');
-  });
+  }), counter);
